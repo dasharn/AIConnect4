380f7ebd7bd855dd8d91172d650f241f54554545
v17971dd <dasharn.dennis@student.manchester.ac.uk>
2023-06-25 15:19:41 +0100

    My game is complete

---
+++ README.md
@@ -0,0 +1,40 @@
+# Connect Four Game
+
+This is a simple implementation of the Connect Four game using Tkinter in Python.
+
+## Game Rules
+
+Connect Four is a two-player game where the players take turns dropping colored discs into a grid. The objective is to be the first to form a horizontal, vertical, or diagonal line of four discs of your color.
+
+## How to Play
+
+1. Run the script and the game window will appear.
+
+2. The game board is displayed as a grid of white rectangles.
+
+3. Player 1 is represented by red discs (X) and Player 2 (the AI) is represented by yellow discs (O).
+
+4. Player 1 starts the game by clicking on the desired column to drop their disc.
+
+5. The AI will automatically make its move after Player 1.
+
+6. The game continues until one player wins by forming a line of four discs or the game ends in a draw.
+
+## Usage
+
+Make sure you have Python installed on your system. Run the script using the following command:
+
+
+The game window will open, and you can start playing by following the on-screen instructions.
+
+## Dependencies
+
+This project requires the Tkinter library, which is usually included with Python installations.
+
+## Contributing
+
+Contributions are welcome! If you have any suggestions or improvements, please submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ main.py
@@ -0,0 +1,218 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Constants for the game
+WIDTH = 7
+HEIGHT = 6
+PLAYER_1 = "X"
+PLAYER_2 = "O"
+
+class ConnectFourGame:
+    def __init__(self):
+        # Create a game board
+        self.game_board = [[' ' for _ in range(WIDTH)] for _ in range(HEIGHT)]
+
+        # Initialize the Tkinter window
+        self.window = tk.Tk()
+        self.window.title("Connect Four")
+
+        # Create the GUI elements
+        self.canvas = tk.Canvas(self.window, width=WIDTH * 80 + 40, height=HEIGHT * 80 + 40, bg="blue")
+        self.canvas.pack()
+
+        # Bind the canvas to a mouse click event
+        self.canvas.bind("<Button-1>", self.make_move)
+
+        # Draw the initial game board
+        self.draw_board()
+
+    def make_move(self, event):
+        column = event.x // 80
+        for row in range(HEIGHT - 1, -1, -1):
+            if self.game_board[row][column] == ' ':
+                self.game_board[row][column] = PLAYER_1
+                self.draw_board()
+                if self.check_winner(row, column, PLAYER_1):
+                    messagebox.showinfo("Game Over", "Player 1 wins!")
+                    self.reset_game()
+                    return
+                if self.check_draw():
+                    messagebox.showinfo("Game Over", "It's a draw!")
+                    self.reset_game()
+                    return
+                self.ai_move()
+                return
+
+    def ai_move(self):
+        best_score = float('-inf')
+        best_move = None
+
+        for col in range(WIDTH):
+            if self.is_valid_move(col):
+                row = self.get_next_row(col)
+                self.game_board[row][col] = PLAYER_2
+
+                score = self.minimax(self.game_board, 4, float('-inf'), float('inf'), False)
+
+                self.game_board[row][col] = ' '
+
+                if score > best_score:
+                    best_score = score
+                    best_move = col
+
+        self.game_board[self.get_next_row(best_move)][best_move] = PLAYER_2
+        self.draw_board()
+
+        if self.check_winner(self.get_next_row(best_move), best_move, PLAYER_2):
+            messagebox.showinfo("Game Over", "Player 2 wins!")
+            self.reset_game()
+            return
+
+        if self.check_draw():
+            messagebox.showinfo("Game Over", "It's a draw!")
+            self.reset_game()
+            return
+
+    def is_valid_move(self, column):
+        return self.game_board[0][column] == ' '
+
+    def get_next_row(self, column):
+        for row in range(HEIGHT - 1, -1, -1):
+            if self.game_board[row][column] == ' ':
+                return row
+
+    def minimax(self, board, depth, alpha, beta, is_maximizing):
+        if self.check_winner(None, None, PLAYER_2):
+            return 1
+        elif self.check_winner(None, None, PLAYER_1):
+            return -1
+        elif self.check_draw():
+            return 0
+
+        if depth == 0:
+            return 0
+
+        if is_maximizing:
+            best_score = float('-inf')
+            for col in range(WIDTH):
+                if self.is_valid_move(col):
+                    row = self.get_next_row(col)
+                    board[row][col] = PLAYER_2
+
+                    score = self.minimax(board, depth - 1, alpha, beta, False)
+
+                    board[row][col] = ' '
+
+                    best_score = max(score, best_score)
+                    alpha = max(alpha, best_score)
+
+                    if beta <= alpha:
+                        break
+            return best_score
+        else:
+            best_score = float('inf')
+            for col in range(WIDTH):
+                if self.is_valid_move(col):
+                    row = self.get_next_row(col)
+                    board[row][col] = PLAYER_1
+
+                    score = self.minimax(board, depth - 1, alpha, beta, True)
+
+                    board[row][col] = ' '
+
+                    best_score = min(score, best_score)
+                    beta = min(beta, best_score)
+
+                    if beta <= alpha:
+                        break
+            return best_score
+
+    def check_winner(self, row, col, player):
+        if row is None and col is None:
+            for r in range(HEIGHT):
+                for c in range(WIDTH):
+                    if self.game_board[r][c] == player:
+                        if self.check_line(r, c, player):
+                            return True
+            return False
+        else:
+            return self.check_line(row, col, player)
+
+    def check_line(self, row, col, player):
+        # Check horizontally
+        count = 0
+        for c in range(WIDTH):
+            if self.game_board[row][c] == player:
+                count += 1
+                if count == 4:
+                    return True
+            else:
+                count = 0
+
+        # Check vertically
+        count = 0
+        for r in range(HEIGHT):
+            if self.game_board[r][col] == player:
+                count += 1
+                if count == 4:
+                    return True
+            else:
+                count = 0
+
+        # Check diagonals
+        for r in range(HEIGHT - 3):
+            for c in range(WIDTH - 3):
+                if (
+                    self.game_board[r][c] == player and
+                    self.game_board[r+1][c+1] == player and
+                    self.game_board[r+2][c+2] == player and
+                    self.game_board[r+3][c+3] == player
+                ):
+                    return True
+
+        for r in range(3, HEIGHT):
+            for c in range(WIDTH - 3):
+                if (
+                    self.game_board[r][c] == player and
+                    self.game_board[r-1][c+1] == player and
+                    self.game_board[r-2][c+2] == player and
+                    self.game_board[r-3][c+3] == player
+                ):
+                    return True
+
+        return False
+
+    def check_draw(self):
+        for row in range(HEIGHT):
+            for col in range(WIDTH):
+                if self.game_board[row][col] == ' ':
+                    return False
+        return True
+
+    def draw_board(self):
+        self.canvas.delete("all")
+        for row in range(HEIGHT):
+            for col in range(WIDTH):
+                x1 = col * 80 + 20
+                y1 = row * 80 + 20
+                x2 = x1 + 60
+                y2 = y1 + 60
+                self.canvas.create_rectangle(x1, y1, x2, y2, fill="white", outline="black")
+                if self.game_board[row][col] == PLAYER_1:
+                    self.canvas.create_oval(x1, y1, x2, y2, fill="red", outline="red")
+                elif self.game_board[row][col] == PLAYER_2:
+                    self.canvas.create_oval(x1, y1, x2, y2, fill="yellow", outline="yellow")
+
+    def reset_game(self):
+        for row in range(HEIGHT):
+            for col in range(WIDTH):
+                self.game_board[row][col] = ' '
+        self.draw_board()
+
+    def start(self):
+        # Start the Tkinter event loop
+        self.window.mainloop()
+
+# Create an instance of the game
+game = ConnectFourGame()
+game.start()
